e7a36280e1353f4db2beb5d66b2e8e68a823d0ed
Ady Nugroho <hooptears@gmail.com>
2024-04-21 16:01:49 +0700

    test2

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -6,6 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe odit placeat nesciunt officiis modi nemo maiores tempora, odio aperiam, nam quis asperiores rem quos quod explicabo eos incidunt sint quidem?</p> 
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe odit placeat nesciunt officiis modi nemo maiores tempora, odio aperiam, nam quis asperiores rem quos quod explicabo eos incidunt sint quidem??</p> 
 </body>
 </html>
